d6fed4fa1e6319422c99b2f1df2885c75f331142
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2022-03-19 12:03:57 +0530

    Time: 6 ms (94.20%), Space: 10.2 MB (12.37%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -4,9 +4,17 @@ public:
         if (c.size() <= 2)
             return true;
         
-        for (int i = 1; i < c.size() - 1; ++i)
-            if ((c[i][1] - c[i - 1][1]) * (c[i + 1][0] - c[i][0]) != (c[i + 1][1] - c[i][1]) * (c[i][0] - c[i - 1][0]))
-                return false;
+        int xd = c[1][0] - c[0][0]; 
+        int yd = c[1][1] - c[0][1]; 
+        
+        for (int i = 2; i < c.size(); ++i) 
+        {
+            int x1 = c[i][0] - c[0][0];
+            int y1 = c[i][1] - c[0][1];
+            
+            if(x1 * yd != y1 * xd) 
+                return false; 
+        }
         return true;
     }
 };
