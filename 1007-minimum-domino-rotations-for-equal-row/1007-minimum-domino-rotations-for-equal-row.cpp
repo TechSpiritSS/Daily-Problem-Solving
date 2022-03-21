97f2a2cb256d9bc66d9affc08b0008baca30de13
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2022-03-21 16:38:16 +0530

    Time: 135 ms (79.47%), Space: 111.5 MB (72.54%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -22,36 +22,12 @@ class Solution {
     }
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        int common = 0;
-        int common2 = 0;
         
-        if (tops[0] == tops[1] || tops[0] == bottoms[1])
-            common = tops[0];
+        int ans = check(tops, bottoms, tops[0]);
         
-        if (bottoms[0] == tops[1] || bottoms[0] == bottoms[1])
-            common2 = bottoms[0];
-        
-        bool both = false;
-        
-        if (common && common2)
-            both = true;
-        
-        else if (common2)
-            common = common2;
-        
-        
-        int ans = check(tops, bottoms, common);
-        int ans2 = -1;
-        
-        if (both)
-            ans2 = check(tops, bottoms, common2);
+        if (ans == - 1)
+            ans = check(tops, bottoms, bottoms[0]);
        
-        if (ans2 == -1)
-            return ans;
-        
-        if (ans == -1)
-            return ans2;
-        
-        return min(ans, ans2);
-    }
+        return ans;
+       }
 };
