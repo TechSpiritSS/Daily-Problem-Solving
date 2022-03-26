16154c0938316c439495475e6828d886f2cfd216
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2022-03-26 22:03:42 +0530

    Time: 17 ms (34.44%), Space: 10.7 MB (29.47%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,5 +1,4 @@
 class Solution {
-    
     static int bits(int x)
     {
         int count = 0;
@@ -14,19 +13,27 @@ class Solution {
         return count;
     }
     
-    static bool cmp(int a, int b)
+    static bool cmp(pair<int, int> a, pair<int, int> b)
     {
-        if (bits(a) == bits(b))
-            return a < b;
-        
-        if (bits(a) < bits(b))
-            return true;
-        
-        return false;
+        if (a.second != b.second)
+            return a.second < b.second;   
+        return a.first < b.first;
     }
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        sort(arr.begin(), arr.end(), cmp);
-        return arr;
+        vector<pair<int, int>> bit_count;
+        
+        for (int &i : arr)
+            bit_count.push_back({i, bits(i)});
+        
+   
+        sort(bit_count.begin(), bit_count.end(), cmp);
+        
+        vector<int> ans;
+        
+        for (pair<int, int> &i : bit_count)
+            ans.push_back(i.first);
+        
+        return ans;
     }
 };
