9ecac9ab3173ec0de684326497ae7ac3f827c14e
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2022-03-26 21:42:18 +0530

    Time: 36 ms (88.84%), Space: 27.6 MB (63.85%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -4,12 +4,12 @@ public:
         int low = 0;
         int high = nums.size() - 1;
         
-        while(low <= high)
+        while (low <= high)
         {
             int mid = (low + high) / 2;
-            if(target == nums[mid])
+            if (target == nums[mid])
                 return mid;
-            else if( target < nums[mid])
+            else if ( target < nums[mid])
                 high = mid - 1;
             else
                 low = mid + 1;
