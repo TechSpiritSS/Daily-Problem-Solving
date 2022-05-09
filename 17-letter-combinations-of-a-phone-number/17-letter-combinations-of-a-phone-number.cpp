c81da71589d3d3aa7599b6393f6b752977a7718e
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2022-05-09 18:00:33 +0530

    Time: 5 ms (16.72%), Space: 6.5 MB (84.83%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        int n = 1;
+        
+        if (digits.size() == 0)
+            return ans;
+        
+        unordered_map <int, string> dial;
+        dial[2] = "abc";
+        dial[3] = "def";
+        dial[4] = "ghi";
+        dial[5] = "jkl";
+        dial[6] = "mno";
+        dial[7] = "pqrs";
+        dial[8] = "tuv";
+        dial[9] = "wxyz";
+        
+        for (char &c : digits)
+            if (c == '7' || c == '9')
+                n *= 4;
+            else
+                n *= 3;
+        
+        ans.resize(n);
+        
+        for (char &c : digits)
+        {
+            int p = n / dial[c - '0'].size();
+            for (int j = 0; j < ans.size();)
+                for (int k = 0; k < dial[c - '0'].size(); ++k)
+                {
+                    int tmp = p;
+                    while (tmp--)
+                    {
+                        ans[j] += dial[c - '0'][k];
+                        ++j;
+                    }
+                }
+            n = p;
+        }
+        return ans;
+    }
+};
