d5b62ed7cda5626899e7eb97affe41ede9e4c643
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2022-05-18 14:39:38 +0530

    Time: 28 ms (92.20%), Space: 8.2 MB (72.36%) - LeetHub

--- 1534-count-good-triplets/1534-count-good-triplets.cpp
+++ 1534-count-good-triplets/1534-count-good-triplets.cpp
@@ -1,20 +1,14 @@
 class Solution {
-    bool check(int i, int j, int k, int a, int b, int c)
-    {
-        bool x = abs(i - j) <= a;
-        x &= abs(j - k) <= b;
-        x &= abs(i - k) <= c;
-        return x;
-    }
 public:
     int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
         int ans = 0;
         
         for (int i = 0; i < arr.size(); ++i)
             for (int j = i + 1; j < arr.size(); ++j)
-                for (int k = j + 1; k < arr.size(); ++k)
-                    if (check(arr[i], arr[j], arr[k], a, b, c))
-                        ++ans;
+                if (abs(arr[i] - arr[j]) <= a)
+                    for (int k = j + 1; k < arr.size(); ++k)
+                        if (abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c)
+                            ++ans;
         return ans;
     }
 };
