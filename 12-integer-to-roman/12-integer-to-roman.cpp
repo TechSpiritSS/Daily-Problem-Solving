bf394a33b4f3983a2815c07335e99189b27e4c59
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2022-06-08 14:48:24 +0530

    Time: 8 ms (70.75%), Space: 8.7 MB (28.19%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    vector<string> symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    vector<int> value = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        
+public:
+    string intToRoman(int num) {
+        string ans = "";
+        
+        for (int i = 0; i < value.size(); ++i)
+            while (value[i] <= num)
+            {
+                ans += symbols[i];
+                num -= value[i];
+            }
+        
+        return ans;
+    }
+};
