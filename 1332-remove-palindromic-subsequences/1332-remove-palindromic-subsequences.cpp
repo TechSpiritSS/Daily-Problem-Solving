ec2af21bb8d46ffbc8137cf1ffaa1d6f60968349
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2022-06-08 14:27:50 +0530

    Time: 5 ms (17.09%), Space: 6.2 MB (78.83%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -1,26 +1,14 @@
 class Solution {
-    bool a = false, b = false;
     bool palindrome(string s)
     {
         int n = s.size() - 1;
         for (int i = 0, j = n; i < j; ++i, --j)
-        {
-            if (s[i] == 'a' || s[j] == 'a')
-                a = true;
-            if (s[i] == 'b' || s[j] == 'b')
-                b = true;
             if (s[i] != s[j])
                 return false;
-        }
         return true;
     }
 public:
     int removePalindromeSub(string s) {
-        if (palindrome(s))
-            return 1;
-        
-        if (a & b)
-            return 2;
-        return 1;
+        return palindrome(s) ? 1 : 2;
     }
 };
