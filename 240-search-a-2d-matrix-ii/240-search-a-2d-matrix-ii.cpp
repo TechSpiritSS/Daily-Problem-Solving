dac2e44bc1c821854f58cd850539e35730412a86
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2022-07-24 15:24:40 +0530

    Time: 1413 ms (5.02%), Space: 14.9 MB (21.86%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for (auto &i : matrix)
+            for (auto &j : i)
+                if (j == target)
+                    return true;
+        return false;
+    }
+};
