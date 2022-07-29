62353e0e6f4f8e0d50a0b5f31bb6f7cd8a7a2ad1
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2022-07-29 17:11:19 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (90.25%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int path[m][n];
+        //nCr
+        int N = m + n - 2;
+        int r = min(m - 1, n - 1);
+        double res = 1;
         
-        for (int i = 0;i < m; ++i)
-            path[i][0] = 1;
+        for (int i = 1; i <= r; ++i)
+            res = res * ( N - r + i) / i;
         
-        for (int i = 0;i < n; ++i)
-            path[0][i] = 1;
-    
-        for (int i = 1; i < m; ++i)
-            for (int j = 1; j < n; ++j)
-                path[i][j] = path[i - 1][j] + path[i][j - 1];
-        
-        return path[m - 1][n - 1];
+        return (int)res;
     }
 };
