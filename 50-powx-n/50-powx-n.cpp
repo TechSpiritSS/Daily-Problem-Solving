50bc9c5d496448c3d9d9c3324046e7ae054295d4
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2022-09-04 19:49:49 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (46.07%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double res = 1;
+        while (n)
+        {
+           if ((n & 1) && n >= 0) res *= x;
+           else if ((n & 1) && n < 0) res = res/x;
+           x *= x;
+           n /= 2;
+        } 
+        return res;
+    }    
+};
