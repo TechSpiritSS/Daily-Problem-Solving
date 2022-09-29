722ee51b7b57222d0dcbd22776fa13ebbb4e545e
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2022-09-29 22:01:56 +0530

    Time: 44 ms (94.96%), Space: 31.9 MB (61.64%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        vector<int> ans;
+        int i = lower_bound(arr.begin(), arr.end(), x) - arr.begin() - 1;
+		int j = i + 1;
+		vector<int> ans;
+
 		while (k--)
 		{
-            auto it = lower_bound(arr.begin(), arr.end(), x);
-			if (it == arr.begin())
+			if (i >= 0 && j < arr.size())
 			{
-				ans.push_back(arr[0]);
-				arr.erase(arr.begin());
-			}
-			else if (it == arr.end())
-			{
-				ans.push_back(arr.back());
-				arr.pop_back();
-			}
-			else
-			{
-				if (x - *(it - 1) <= *it - x)
+				if (abs(arr[i] - x) <= abs(arr[j] - x))
 				{
-					ans.push_back(*(it - 1));
-					arr.erase(it - 1);
+					ans.push_back(arr[i]);
+					--i;
 				}
 				else
 				{
-					ans.push_back(*it);
-					arr.erase(it);
+					ans.push_back(arr[j]);
+					++j;
 				}
+			}
+			else if (i >= 0)
+			{
+				ans.push_back(arr[i]);
+				--i;
+			}
+			else
+			{
+				ans.push_back(arr[j]);
+				++j;
 			}
 		}
 		sort(ans.begin(), ans.end());
