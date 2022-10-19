49868a651decd32b52cf5d91b139e713609e1821
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2022-10-19 16:50:19 +0530

    Time: 40 ms (7.92%), Space: 12.1 MB (99.46%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,18 @@
+class Solution {    
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int> f;
+        for (auto &s : words) ++f[s];
+        
+        sort(words.begin(), words.end(), [&f](string &a, string&b)
+             {
+                 if (f[a] > f[b]) return true;
+                 if (f[a] < f[b]) return false;
+                 return a < b;
+             });
+        
+        unique(words.begin(), words.end());
+        
+        return vector<string> (words.begin(), words.begin() + k);
+    }
+};
