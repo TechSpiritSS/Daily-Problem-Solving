c3014f9aa7d7fb2e9b2b5c7323113915741629a4
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2022-10-19 16:50:37 +0530

    Time: 40 ms (7.92%), Space: 12.1 MB (99.80%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -4,15 +4,13 @@ public:
         unordered_map<string, int> f;
         for (auto &s : words) ++f[s];
         
-        sort(words.begin(), words.end(), [&f](string &a, string&b)
-             {
+        sort(words.begin(), words.end(), [&f](string &a, string&b) {
                  if (f[a] > f[b]) return true;
                  if (f[a] < f[b]) return false;
                  return a < b;
              });
         
-        unique(words.begin(), words.end());
-        
+        unique(words.begin(), words.end());       
         return vector<string> (words.begin(), words.begin() + k);
     }
 };
