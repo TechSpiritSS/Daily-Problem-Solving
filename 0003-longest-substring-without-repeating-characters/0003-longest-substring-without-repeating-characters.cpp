fa169191e9fb43c697afbfa85f3e3c3aa5b58ec3
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2022-10-24 23:42:20 +0530

    Time: 18 ms (77.94%), Space: 8.3 MB (60.99%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,8 +5,7 @@ public:
 		unordered_map<char, int> frq;
 		int start = 0, ans = 0;
 
-		for (int i = 0; i < s.size(); ++i)
-		{
+		for (int i = 0; i < s.size(); ++i) {
 			if (frq.count(s[i]))  start = max(start, frq[s[i]] + 1);
 
 			frq[s[i]] = i;
