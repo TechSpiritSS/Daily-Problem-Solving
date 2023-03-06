526f2bd77d65234f128cd634a8a266d18bd3428c
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2023-03-06 14:45:21 +0530

    Time: 12 ms (82.74%), Space: 8.8 MB (47.61%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> mag(26);
+        
+        for (auto &c : magazine)
+            ++mag[c - 'a'];
+        
+        for (auto &c : ransomNote)
+            if (--mag[c - 'a'] < 0)
+                return false;
+        
+        return true;
+    }
+};
