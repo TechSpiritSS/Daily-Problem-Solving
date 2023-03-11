d5ea2c27be1b1ee4d6ef586edb7947da2fd80af8
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2023-03-11 23:42:24 +0530

    Time: 5 ms (58.21%), Space: 16.5 MB (25.51%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -10,14 +10,14 @@
  * };
  */
 class Solution {
-    bool check(TreeNode* L, TreeNode* R)
+    bool check(TreeNode *L, TreeNode *R)
     {
         if (!L && !R) return true;
         if (!L || !R) return false;
         if (L -> val != R -> val) return false;
         return check(L -> left, R -> right) && check(L -> right, R -> left);
-            
     }
+    
 public:
     bool isSymmetric(TreeNode* root) {
         if (!root) return true;
