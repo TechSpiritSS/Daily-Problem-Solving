50c3d94806b9a9eec15803a27256443d96e19622
Sidharth Sethi <76687985+TechSpiritSS@users.noreply.github.com>
2023-04-26 20:18:02 +0530

    Time: 7 ms (16.24%), Space: 5.9 MB (63.10%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int addDigits(int num) {
-        if (num == 0) return 0;
+        if (num == 0) 
+            return 0;
         return (num % 9) ? num % 9 : 9;
     }
 };
